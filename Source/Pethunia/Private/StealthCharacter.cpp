0e1461bd509187e97c0ce6308278b671719e2a0e
KnighTzZz1 <40316156+KnighTzZz1@users.noreply.github.com>
2019-10-29 10:52:26 +0400

    Stealth Character Blank Code

---
+++ Source/Pethunia/Private/StealthCharacter.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StealthCharacter.h"
+
+void AStealthCharacter::Tick(float DeltaTime)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Tick Working"));
+}
